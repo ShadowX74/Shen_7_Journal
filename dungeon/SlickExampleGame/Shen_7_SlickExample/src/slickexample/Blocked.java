c19bed82e85afebb568c50b3021b3dbb79d2dcf6
Xiangyu Shen <xshen2013@gmail.com>
2016-01-06 16:04:19 -0600

    Update

---
+++ dungeon/SlickExampleGame/Shen_7_SlickExample/src/slickexample/Blocked.java
@@ -0,0 +1,23 @@
+package slickexample;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ShadowX
+ */
+public class Blocked {
+
+	public static boolean[][] blocked;
+
+	public static boolean[][] getblocked() {
+
+		return blocked;
+
+	}
+
+}
