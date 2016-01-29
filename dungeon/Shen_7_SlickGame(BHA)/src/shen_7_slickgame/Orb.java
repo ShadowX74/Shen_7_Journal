ba511d85f5679b66f62454f44a15301f1ab4c7dc
Xiangyu Shen <xshen2013@gmail.com>
2016-01-29 15:53:04 -0600

    Updates

--- dungeon/Shen_7_SlickGame(BHA)/src/shen_7_slickgame/Orb.java
+++ dungeon/Shen_7_SlickGame(BHA)/src/shen_7_slickgame/Orb.java
@@ -15,16 +15,98 @@ import org.newdawn.slick.geom.Shape;
  * @author ShadowX
  */
 public class Orb {
-        private int locationX; 
-	private int locationY;
-        private int size;
+        private int x, y, width, height; 
+        private int dmg, hitboxX, hitboxY;
         private boolean isVisible;
-	Image orb;
-        public Orb(int x, int y) {
+	Image orbpic;
+        Shape hitbox;
+        public Orb(int a, int b) throws SlickException {
+            this.x = x;
+            this.y = y;
             this.isVisible = false;
-            this.locationX = x;
-            this.locationY = y;
+            this.orbpic = new Image("res/orbs/Ninja_12");
+            this.hitbox = new Rectangle (a, b, 32, 32);
         }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getDmg() {
+        return dmg;
+    }
+
+    public void setDmg(int dmg) {
+        this.dmg = dmg;
+    }
+
+    public int getHitboxX() {
+        return hitboxX;
+    }
+
+    public void setHitboxX(int hitboxX) {
+        this.hitboxX = hitboxX;
+    }
+
+    public int getHitboxY() {
+        return hitboxY;
+    }
+
+    public void setHitboxY(int hitboxY) {
+        this.hitboxY = hitboxY;
+    }
+
+    public boolean isIsVisible() {
+        return isVisible;
+    }
+
+    public void setIsVisible(boolean isVisible) {
+        this.isVisible = isVisible;
+    }
+
+    public Image getOrbpic() {
+        return orbpic;
+    }
+
+    public void setOrbpic(Image orbpic) {
+        this.orbpic = orbpic;
+    }
+
+    public Shape getHitbox() {
+        return hitbox;
+    }
+
+    public void setHitbox(Shape hitbox) {
+        this.hitbox = hitbox;
+    }
         /**
          * Getters and setters are a common concept in Java.
          * A design guideline in Java, and object oriented programming in 
@@ -32,6 +114,6 @@ public class Orb {
          * Getters - are methods used to query the value of instance variables.
          * this.getLocationX();
          * Setters - methods that set values for instance variables
-         * this.setLocationX(45)
+         * this.setLocation(Player.x, Player.y)
          */
 }
